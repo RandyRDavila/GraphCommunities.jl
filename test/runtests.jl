5b75f917283d51bdea084bd9499fdbf7f03672c2
RandyRDavila <randyrdavila@gmail.com>
2023-09-25 15:05:32 -0500

    fixed unit tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,9 +4,14 @@ using Graphs
 
 @testset "GraphCommunities.jl Tests" begin
 
-    @testset "karate_club_graph" begin
-        g = karate_club_graph()
+    @testset "generate: KarateClub()" begin
+        g = generate(KarateClub())
         @test nv(g) == 34
+    end
+
+    @testset "generate: PlantedPartition()" begin
+        g = generate(PlantedPartition(2, 3, 0.5, 0.1))
+        @test nv(g) == 6
     end
 
     @testset "community_detection: Louvain()" begin
@@ -15,18 +20,20 @@ using Graphs
         add_edge!(g, 2, 3)
         add_edge!(g, 3, 1)
         communities = community_detection(g, Louvain())
+
         @test length(communities) == 3
+        @test length(unique(values(communities))) == 1
     end
 
     @testset "community_detection: KClique()" begin
-        g = chained_cliques_graph(2, 3)
+        g = generate(ChainedCliques(2, 6))
         communities = community_detection(g, KClique())
         @test length(unique(values(communities))) == 2
     end
 
-    @testset "planted_partition_graph" begin
-        g = planted_partition_graph(2, 3, 0.5, 0.1)
-        @test nv(g) == 6
+    @testset "community_detection: LabelPropagation()" begin
+        g = generate(ChainedCliques(2, 6))
+        communities = community_detection(g, LabelPropagation())
+        @test length(unique(values(communities))) == 2
     end
-
 end
