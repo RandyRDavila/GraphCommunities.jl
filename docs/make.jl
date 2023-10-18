e88c6111b7b48848617f76210603e8b30cee2647
RandyRDavila <randyrdavila@gmail.com>
2023-10-17 19:09:21 -0500

    adding automation

--- docs/make.jl
+++ docs/make.jl
@@ -1,9 +1,6 @@
 using Documenter
 using GraphCommunities
 
-
-
-
 makedocs(
     sitename="GraphCommunities.jl",
     modules=[GraphCommunities],
@@ -11,21 +8,21 @@ makedocs(
     checkdocs=:none,
 )
 
-deploydocs(
-    root = "GraphCommunities",
-    target = "build",
-    dirname = "",
-    repo = "https://github.com/RandyRDavila/GraphCommunities.jl.git",
-    branch = "gh-pages",
-    deps = nothing,
-    make = nothing,
-    devbranch = nothing,
-    devurl = "dev",
-    versions = ["stable" => "v^", "v#.#", devurl => devurl],
-    forcepush = false,
-    deploy_config = auto_detect_deploy_system(),
-    push_preview = false,
-    repo_previews = repo,
-    branch_previews = branch,
-    tag_prefix = "",
-)
+# deploydocs(
+#     root = "GraphCommunities",
+#     target = "build",
+#     dirname = "",
+#     repo = "https://github.com/RandyRDavila/GraphCommunities.jl.git",
+#     branch = "gh-pages",
+#     deps = nothing,
+#     make = nothing,
+#     devbranch = nothing,
+#     devurl = "dev",
+#     versions = ["stable" => "v^", "v#.#", devurl => devurl],
+#     forcepush = false,
+#     deploy_config = auto_detect_deploy_system(),
+#     push_preview = false,
+#     repo_previews = repo,
+#     branch_previews = branch,
+#     tag_prefix = "",
+# )
