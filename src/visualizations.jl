a93fccbc5f5e68721eb27bff986600833b9c5dc8
Randy Davila <33989989+RandyRDavila@users.noreply.github.com>
2023-10-27 12:57:02 -0500

    first commit (#13)
    Co-authored-by: RandyRDavila <davilar@uhd.edu>

--- src/visualizations.jl
+++ src/visualizations.jl
@@ -30,6 +30,35 @@ function draw_communities(g::AbstractGraph, communities::Vector{Int})
     _draw_communities(g, community_dict)
 end
 
+"""
+    draw_communities(g::AbstractGraph, communities::Dict{Int, Int})
+
+Internal function to draw the graph `g` with nodes colored based on their community assignments.
+
+This function maps each unique community label to a distinct color and then generates a plot where each node in the graph `g` is colored according to its community label.
+
+# Arguments
+- `g::AbstractGraph`: The input graph.
+- `communities::Dict{Int, Int}`: A dictionary mapping each vertex to its community label.
+
+# Returns
+- A plot object with nodes colored based on their community label. Each unique community is represented by a unique color.
+
+# Notes
+- This function is an internal helper and is typically not called directly by users. It is used by higher-level community drawing functions which prepare and pass the community mapping in the form of a dictionary.
+- The function assumes that every vertex in the graph `g` is assigned a community label in the `communities` dictionary. If any vertex is missing a label, an error will be raised in the calling function.
+- The colors are chosen to be as distinguishable as possible, but for graphs with a very large number of communities, some colors may be similar.
+
+# Example
+
+```julia
+julia> g = generate(KarateClub())
+
+julia> communities = compute(LabelPropagation(), g)
+
+julia> draw_communities(g, communities)
+```
+"""
 function _draw_communities(g::AbstractGraph, communities::Dict{Int, Int})
     # Map each unique community to a color.
     unique_communities = unique(values(communities))
@@ -42,4 +71,46 @@ function _draw_communities(g::AbstractGraph, communities::Dict{Int, Int})
 
     # Generate the plot.
     gplot(g, nodefillc=node_colors)
+end
+
+"""
+    draw_communities(g::AbstractGraph, node_labels::Vector{Tuple{Int, Int}})
+
+Draw the graph `g` with nodes colored based on their label assignments.
+
+# Arguments
+
+- `g::AbstractGraph`: The input graph.
+- `node_labels::Vector{Tuple{Int, Int}}`: A vector of tuples, each containing a node and its label.
+
+# Returns
+
+- A plot with nodes colored based on their labels.
+
+# Example
+
+```julia
+julia> g = generate(KarateClub())
+
+julia> communities = compute(FastLPA(), g)
+
+julia> draw_communities(g, communities)
+```
+
+# Note
+
+This function will only work if each node in the graph is included in the `node_labels` vector.
+"""
+function draw_communities(g::AbstractGraph, node_labels::Vector{Tuple{Int, Int}})
+    # Convert the Vector of tuples to a Dict.
+    label_dict = Dict{Int, Int}(node_labels)
+
+    # Ensure every vertex is in the label_dict.
+    for v in vertices(g)
+        if !haskey(label_dict, v)
+            throw(ArgumentError("Node $v doesn't have a label assignment!"))
+        end
+    end
+
+    _draw_communities(g, label_dict)
 end
